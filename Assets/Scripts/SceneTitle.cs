67dd555f44a2ac5e7eedd819f095022b4c403a14
hawk_wei <hawk_wei@htc.com>
2021-04-15 12:02:39 +0800

    Debug.Log  demo

---
+++ Assets/Scripts/SceneTitle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneTitle : MonoBehaviour
+{
+    private string sceneName;
+
+    private void Awake()
+    {
+        sceneName = SceneManager.GetActiveScene().name;
+        Debug.LogFormat("{0} {1} {2}", "AAA", "BBB", "CCC");
+        Debug.Log("1 Start:   sceneName=" + sceneName);
+        Debug.Log($"2 Start:   sceneName={sceneName}");
+        Debug.Log(string.Format("Format   sceneName={0}", sceneName));
+        Debug.Log(10.ToString("X"));
+        Debug.Log(String.Format("HEX={0:X}", 10));
+        if(GetComponent<Text>() != null )
+            GetComponent<Text>().text = sceneName;
+        else
+            Debug.Log("GetComponent<Text>() is null");
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Text>().text = sceneName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
