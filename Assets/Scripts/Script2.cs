b8892ea20835d4c2d7ea684daf839e5ad955fa3a
ha271923 <ha271923@gmail.com>
2021-04-25 00:54:13 +0800

    Scripts sequence debug msg

---
+++ Assets/Scripts/Script2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		// Debug.Log("this.name=" + this.name); // object name
+		Debug.Log("this.GetType()=" + this.GetType()); // script name
+		// Debug.Log("this.GetType().Name=" + this.GetType().Name); // script name
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
