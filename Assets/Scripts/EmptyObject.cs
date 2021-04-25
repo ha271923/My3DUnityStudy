e469a7f3047aef54656ad2cb32088fcbc1ef3fb1
ha271923 <ha271923@gmail.com>
2021-04-25 22:48:19 +0800

    find child gameobject at next layer

--- Assets/Scripts/EmptyObject.cs
+++ Assets/Scripts/EmptyObject.cs
@@ -15,5 +15,17 @@ public class EmptyObject : MonoBehaviour
 			if (parentGameObject != null) // =L1
 				Debug.Log("HAWK   parentGameObject.transform.name=" + parentGameObject.transform.name);
 		}
+
+		var childCount = transform.childCount;
+		if (childCount != 0) {
+			for (int i = 0; i < childCount; i++) {
+				if (transform.GetChild(i).gameObject.transform.name == "L2.2")
+				{
+					Debug.Log("HAWK   found childGO L2.2 !!!!!!");
+				}
+			}
+
+		}
+	
 	}
 }
