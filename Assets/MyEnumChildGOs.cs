01c1a1825f8b098e3d098ea2d8c0d08a6551853b
ha271923 <ha271923@gmail.com>
2021-04-29 23:52:55 +0800

    print Child GOs

---
+++ Assets/MyEnumChildGOs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyEnumChildGOs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		printChildGOs();
+
+	}
+
+	private void printChildGOs() {
+		var childCount = transform.childCount;
+		if (childCount != 0) {
+			for (int i = 0; i<childCount; i++) {
+				Debug.Log("transform.GetChild("+i+").gameObject.transform.name=" +
+					transform.GetChild(i).gameObject.transform.name);
+				
+			}
+
+		}
+	}
+}
