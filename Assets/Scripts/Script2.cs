8072fe4976ab6764091a7380fbd851692e153d5d
hawk_wei <hawk_wei@htc.com>
2021-04-29 16:47:50 +0800

    Research Script variables  between objects

--- Assets/Scripts/Script2.cs
+++ Assets/Scripts/Script2.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Script2 : MonoBehaviour
 {
+    GameObject L2Parent;
     // Start is called before the first frame update
     void Start()
     {
 		// Debug.Log("this.name=" + this.name); // object name
 		Debug.Log("this.GetType()=" + this.GetType()); // script name
-		// Debug.Log("this.GetType().Name=" + this.GetType().Name); // script name
-	}
+        // Debug.Log("this.GetType().Name=" + this.GetType().Name); // script name
+        L2Parent.SetActive(true);
+        Debug.Log("L2Parent=" + L2Parent);
+    }
 
     // Update is called once per frame
     void Update()
--- Assets/Scripts/Script3.cs
+++ Assets/Scripts/Script3.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Script3 : MonoBehaviour
 {
+    public GameObject sharedGameObject;
+    int Val = 0;
+    static int sVal = 0;
+
     // Start is called before the first frame update
     void Start()
     {
 		Debug.Log("this.GetType()=" + this.GetType()); // script name
 	}
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        Val++;
+        sVal++;
+
+        if (sharedGameObject != null)
+        {
+            Script1 script1component = sharedGameObject.GetComponent<Script1>();
+            if (script1component != null )
+            {
+                script1component.Val += 1000;
+                Script1.sVal += 7000;
+                Debug.Log("GO name=" + transform.name + "   Script name=" + this.GetType() + "   sVal=" + sVal + "   Val=" + Val);
+            }
+        }
     }
+
 }
