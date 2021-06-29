f132347538772c6cba732288edd8a3900eef6ffe
hawk_wei <hawk_wei@htc.com>
2021-06-29 18:23:02 +0800

    Test Script run seqence

---
+++ Assets/Scripts/ParentChilds/Script2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyGOs
+{
+
+    public class Script2 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("this=" + this);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/ParentChilds/Script7.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyGOs
+{
+
+    public class Script7 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("this=" + this);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
