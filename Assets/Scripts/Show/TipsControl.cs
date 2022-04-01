1d845b27f44f356716ada65586e7459929504330
hawk_wei <hawk_wei@htc.com>
2022-04-01 17:11:36 +0800

    show tooltip

---
+++ Assets/Scripts/Show/TipsControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TipsControl : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject tipsPrefab;
+    private GameObject tipsGO;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tipsGO = Instantiate(tipsPrefab, new Vector3(30, 30, 0), Quaternion.identity);
+        tipsGO.SetActive(false);
+        tipsGO.transform.SetParent(this.transform, false);
+    }
+
+    public void OnPointerEnter(PointerEventData data)
+    {
+        Debug.LogWarning("OnPointerEnter");
+
+        tipsGO.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData data)
+    {
+        Debug.LogWarning("OnPointerExit");
+        tipsGO.SetActive(false);
+    }
+}
