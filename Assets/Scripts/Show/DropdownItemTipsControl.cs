d7181d7b646c7b104b8e6e611a3ac6ddab1400db
hawk_wei <hawk_wei@htc.com>
2022-04-06 15:57:52 +0800

    Add Tips control scripts

---
+++ Assets/Scripts/Show/DropdownItemTipsControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownItemTipsControl : MonoBehaviour
+{
+    public void OnDropdownItemEnter()
+    {
+        Text text = this.GetComponentInChildren<Text>();
+        // Debug.LogWarning("OnDropdownItemEnter+   value=" + value);
+        // 
+        // Debug.LogWarning("OnDropdownItemEnter-   value=" + value);
+
+        Debug.LogWarning("OnDropdownItemEnter   text."+ text.text);
+    }
+
+    public void OnDropdownItemExit()
+    {
+        Text text = this.GetComponentInChildren<Text>();
+        // int value = dropdown.value;
+        // Debug.LogWarning("OnDropdownItemExit+   value=" + value);
+        // 
+        // Debug.LogWarning("OnDropdownItemExit-   value=" + value);
+
+        Debug.LogWarning("OnDropdownItemExit   text." + text.text);
+    }
+}
---
+++ Assets/Scripts/Show/DropdownTipsControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownTipsControl : MonoBehaviour
+{
+    public Dropdown dropdown;
+
+    // Update is called once per frame
+    void Start()
+    {
+
+    }
+
+
+
+}
