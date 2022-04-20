d7d415f7798881581c8a9b2904717a69290a03a7
hawk_wei <hawk_wei@htc.com>
2022-04-20 21:25:57 +0800

    add some scene and test code

---
+++ Assets/Scripts/ScriptableObjects/DropdownTest.cs
@@ -0,0 +1,45 @@
+﻿using I2.Loc;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using static TMPro.TMP_Dropdown;
+
+public class DropdownTest : MonoBehaviour
+{
+    static public List<OptionData> sInteractionMethodOptions = new List<OptionData>();
+
+    TMPro.TMP_Dropdown interactionMethodDropdown;
+    private void OnEnable()
+    {
+
+        initInteractionMethodOptions2();
+    }
+
+    private void initInteractionMethodOptions()
+    {
+        LocalizedString ls;
+        interactionMethodDropdown = GetComponent<TMPro.TMP_Dropdown>();
+        interactionMethodDropdown.ClearOptions();
+        ls = new LocalizedString(ScriptLocalization.Settings.KIOSK_INTERACTION_MODE_FINCH);
+        OptionData optionData_FINCH = new OptionData(ls);
+        sInteractionMethodOptions.Add(optionData_FINCH);
+        ls = new LocalizedString(ScriptLocalization.Settings.KIOSK_INTERACTION_MODE_PAC);
+        OptionData optionData_PAC = new OptionData(ls);
+        sInteractionMethodOptions.Add(optionData_PAC);
+        ls = new LocalizedString(ScriptLocalization.Settings.KIOSK_INTERACTION_MODE_GAZE);
+        OptionData optionData_GAZE = new OptionData(ls);
+        sInteractionMethodOptions.Add(optionData_GAZE);
+        // interactionMethodDropdown.AddOptions(interactionMethodDropdown);
+    }
+
+    private void initInteractionMethodOptions2()
+    {
+        LocalizedString ls;
+        interactionMethodDropdown = GetComponent<TMPro.TMP_Dropdown>();
+        interactionMethodDropdown.ClearOptions();
+        sInteractionMethodOptions.Add(new TMP_Dropdown.OptionData(ScriptLocalization.Settings.KIOSK_INTERACTION_MODE_FINCH));
+        sInteractionMethodOptions.Add(new TMP_Dropdown.OptionData(ScriptLocalization.Settings.KIOSK_INTERACTION_MODE_PAC));
+        sInteractionMethodOptions.Add(new TMP_Dropdown.OptionData(ScriptLocalization.Settings.KIOSK_INTERACTION_MODE_GAZE));
+        interactionMethodDropdown.AddOptions(sInteractionMethodOptions);
+    }
+}
