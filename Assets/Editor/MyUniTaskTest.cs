2810acb6a3b41c55eb70b4f44f1d865903799c9a
Hawk Wei <hawk_wei@htc.com>
2023-08-09 16:25:58 +0800

    add UniTask SDK

---
+++ Assets/Editor/MyUniTaskTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class MyUniTaskTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void MyUniTaskTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            Debug.Log("MyUniTaskTestSimplePasses ++++++");
+            Debug.Log("MyUniTaskTestSimplePasses ------");
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator MyUniTaskTestWithEnumeratorPasses()
+        {
+            Debug.Log("MyUniTaskTestWithEnumeratorPasses +++");
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+            Debug.Log("MyUniTaskTestWithEnumeratorPasses ---");
+        }
+    }
+}
